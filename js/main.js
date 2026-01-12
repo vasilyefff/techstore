94ae0cab77f1cf703adf995e408f1406527a9e02
vasilyefff <vasilyeff_k@mail.ru>
2026-01-12 14:23:38 +0300

    feat: render cart items with quantity and price
    - Added renderCart function
    - Display cart items list
    - Show quantity and item total price
    - Sync cart UI with localStorage and total sum

--- js/main.js
+++ js/main.js
@@ -2,8 +2,80 @@ import { products } from './data/products.js';
 import { createProductCard } from './components/ProductCard.js';
 
 const productsContainer = document.getElementById('products');
+const cartTotalElement = document.getElementById('cart-total');
+const cartItemsContainer = document.getElementById('cart-items');
 
-products.forEach(product => {
-	productsContainer.innerHTML += createProductCard(product);
+const cart = JSON.parse(localStorage.getItem('cart')) || [];
+updateCartTotal(cart);
+renderCart(cart);
+
+productsContainer.innerHTML = products
+	.map(product => createProductCard(product))
+	.join('');
+
+
+function getCartTotal(cart) {
+	return cart.reduce((total, item) => {
+		return total + item.product.price * item.quantity;
+	}, 0);
+}
+
+function updateCartTotal(cart) {
+	const total = getCartTotal(cart);
+	cartTotalElement.textContent = total.toLocaleString('ru-RU');
+}
+
+function saveCart(cart) {
+	localStorage.setItem('cart', JSON.stringify(cart));
+}
+
+function renderCart(cart) {
+	cartItemsContainer.innerHTML = '';
+
+	cart.forEach(item => {
+		const cartItemHTML = `
+			<div class="cart-item">
+				<span class="cart-item-title">
+					${item.product.title}
+				</span>
+
+				<span class="cart-item-quantity">
+					x ${item.quantity}
+				</span>
+
+				<span class="cart-item-price">
+					${(item.product.price * item.quantity).toLocaleString('ru-RU')} ₽
+				</span>
+			</div>
+		`;
+
+		cartItemsContainer.innerHTML += cartItemHTML;
+	});
+}
+
+
+productsContainer.addEventListener('click', (event) => {
+	const button = event.target;
+
+	if (!button.classList.contains('add-to-cart-btn')) return;
+
+	const productId = Number(button.dataset.id);
+	const product = products.find(item => item.id === productId);
+
+	if (!product) return;
+
+	const cartItem = cart.find(item => item.product.id === product.id);
+
+	if (cartItem) {
+		cartItem.quantity += 1;
+	} else {
+		cart.push({
+			product,
+			quantity: 1,
+		});
+	}
+
+	saveCart(cart);
+	updateCartTotal(cart);
+	renderCart(cart);
 });
-
